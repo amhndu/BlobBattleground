76043b2400d13d45c80f13e48e8c9715911db60f
DevPika <gameshire@gmail.com>
2018-08-19 19:43:03 +0530

    Create rooms

--- game/game_room.py
+++ game/game_room.py
@@ -1,10 +1,11 @@
 import random
 import string
 
+rooms = {} # dict to track active rooms
+
 class GameRoom:
     def __init__(self):
-        self.room_id = ''.join(random.SystemRandom().choice(
-            string.ascii_uppercase) for _ in range(5))
+        self.room_id = self.generate_room_id()
         self.players = []
         self.owner = None
 
@@ -13,3 +14,11 @@ class GameRoom:
 
     def set_owner(self, owner):
         self.owner = owner
+
+    def generate_room_id(self):
+        room_id = ''.join(random.SystemRandom().choice(
+            string.ascii_uppercase) for _ in range(5))
+        if room_id in rooms:
+            self.generate_room_id();
+        else:
+            return room_id
